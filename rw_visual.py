ef0323bc76190df48263535ce9dabbfc24a2256b
Kleyton Eric da Silva <kleytoneric@hotmail.com>
2022-08-02 10:30:56 -0300

    Update rw_visual.py

--- rw_visual.py
+++ rw_visual.py
@@ -4,21 +4,24 @@ from random_walk import RandomWalk
 
 while True:
     # Cria passeios aleatórios e plota os pontos
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
+    # Define o tamanho da janela de plotagem
+    plt.figure(dpi=128, figsize=(15, 7))
 
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
-                cmap=plt.cm.Blues, edgecolor='none', s=10)
+                cmap=plt.cm.Blues, edgecolor='none', s=3)
 
     # Entafizando o primeiro e o último ponto
     plt.scatter(0, 0, c='green', edgecolors='none', s=35)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
-                edgecolors='none', s=350)
+                edgecolors='none', s=35)
 
     # Remove os eixos para uma melhora visualização
     # plt.axes().get_xaxis().set_visible(False)
     # plt.axes().get_yaxis().set_visible(False)
+    plt.axis('off')
 
     plt.show()
 
